871babe4cb1d45ea400b008935c248f28567564c
Aad <aad.pouw@gmail.com>
2021-01-24 13:36:31 +0100

    WpLibrary/src/SvgCreations added

---
+++ WpLibrary/src/SvgCreations/Factory/SvgCreationsOptions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aad Pouw
+ * Date: 11-10-2020
+ * Time: 16:12
+ */
+
+namespace src\SvgCreations\Factory;
+
+
+
+
+trait SvgCreationsOptions
+{
+    use SvgCreationsVars;
+
+}
